548e40f5cff179cc51bd9c1f285d52db78d01b5c
John Reese <jreese@leetcode.net>
2008-12-15 14:53:37 -0500

    Very basic mash functionality.

---
+++ os1/project1/mash.c
@@ -0,0 +1,127 @@
+/**
+ * Copyright (C) 2008	John Reese
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+
+/**
+ * mAsh - my Awesome shell
+ * Very basic pass-through shell.
+ *
+ * @author John Reese
+ */
+
+#define DEBUG 1
+
+#define LINE_SIZE 80 // 80 bytes should be enough for anybody
+#define ARGS_SIZE LINE_SIZE/2
+
+char* error_string = "Error!\n";
+int error_string_len = 7;
+
+/**
+ * "Safe" write: call write() as needed to make sure all bytes are written.
+ */
+void swrite( int file, void *buf, size_t count ) {
+	int written = 0;
+	while ( written < count ) {
+		written += write( file, buf + written, count - written );
+	}
+}
+
+/**
+ * Parse an input line into a list of arguments.
+ */
+int parse_input( char* input, char *args[] ) {
+	int length = 0; // characters input from stdin
+	int count = 0;  // number of arguments parsed
+	int i = 0;      // current position of parsing
+	int space = 1;  // are we parsing whitespace
+
+	length = read( STDIN_FILENO, input, LINE_SIZE );
+
+	do {
+		switch( input[i] ) {
+			// argument delimiters
+			case ' ':
+			case '\t':
+				input[i] = '\0';
+				space = 1;
+				break;
+
+			// end of input
+			case '\n':
+			case '\0':
+				input[i] = '\0';
+				i = length;
+				break;
+
+			// argument content
+			default:
+				if ( space ) {
+					args[count++] = &input[i];
+				}
+				space = 0;
+		}
+	} while( i++ < length );
+
+	args[count] = NULL;
+
+	if ( DEBUG ) {
+		printf( "\n" );
+		for ( i = 0; i < count; i++ ) {
+			printf( "%d: '%s'\n", i, args[i] );
+		}
+		printf( "Total: %d\n", count );
+	}
+
+	return count;
+}
+
+int main( void ) {
+	char input[LINE_SIZE]; // input buffer
+	char *args[ARGS_SIZE]; // array of pointers into input buffer
+
+	int count; // number of arguments
+	int pid;   // process ID from fork()
+	int error; // error from exec()
+
+	while(1) {
+		swrite( STDOUT_FILENO, ">> ", 3 );
+		count = parse_input( input, args );
+
+		if ( count < 1 ) { // EOF from user, quit shell
+			swrite( STDOUT_FILENO, "\nGood bye!\n", 11 );
+			return 0;
+		}
+
+		// Fork to execute command
+		pid = fork();
+
+		if ( DEBUG ) { printf( "Fork pid: %d\n", pid ); }
+
+		if ( pid == 0 ) { // child
+			error = execvp( input, args );
+			perror( "Error" );
+			return 0;
+
+		} else { //parent
+			wait();
+		}
+	}
+
+	return 0;
+}
