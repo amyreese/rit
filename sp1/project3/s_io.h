eb17342a8f02142da97ca80c4d4fda272fd36c23
Adam J Risi <ajr7708@wilson.(none)>
2009-02-21 15:36:44 -0500

    Added the s_io.h stubs for serial io

--- sp1/project3/s_io.h
+++ sp1/project3/s_io.h
@@ -11,9 +11,38 @@
 #define __S_IO_H__
 
 /**
+ * Global external variable def's
+ */
+extern char input[];
+extern unsigned int S_IO_DONE;
+
+/**
  * Initialize the serial I/O controller.
  */
 void s_io_init();
+
+/**
+ * Enables the serial input interrupt and blocks until a single character is read.
+ * Disables serial input interrupt when done
+ * 
+ * @return the unsigned int value of the read char
+ */
+unsigned int s_getchar( void );
+
+/**
+ * Enables the serial interrupt, reads until a \r or \n is found, or until the buffer
+ * is full, disables the serial interrupt, and returns the address of the read string
+ *
+ * @return the read in string
+ */
+char *s_gets( void );
+
+/**
+ * Enables serial interrupt and sends out a string. Disables interrupt when done
+ *
+ * @param str the string to send over serial device
+ */
+void s_puts(char *str);
 
 #endif // __S_IO_H__
 
