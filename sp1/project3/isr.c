94c08baff1edee2b0d115ad8fda04b17986d4a13
Adam J Risi <ajr7708@wilson.(none)>
2009-02-21 17:09:15 -0500

    Added code to the serial isr

--- sp1/project3/isr.c
+++ sp1/project3/isr.c
@@ -6,6 +6,7 @@
  */
 
 #include "isr.h"
+#include "s_io.h"
 #include "c_io.h"
 #include "helper.h"
 #include "startup.h"
@@ -31,8 +32,33 @@ void isr_timer( int vector, int code ) {
  * Serial I/O interrupt service routine.
  */
 void isr_serial( int vector, int code ) {
+  char interrupts = __inb(UA4_EIR) & UA4_EIR_INT_PRI_MASK;
+  if((interrupts & UA4_EIR_NO_INT_PENDING) != 0) {
+  }
 
-	__outw( PIC_MASTER_CMD_PORT, PIC_EOI );
+  if((interrupts & UA4_EIR_LINE_STATUS_INT_PENDING) != 0) {
+  }
+
+  /* if this interrupt trips, then there is a character waiting for us */
+  if((interrupts & UA4_EIR_RX_INT_PENDING) != 0) {
+    *input = __inb(UA4_RX_DATA) & 0x7f;
+    read_characters = 1;
+  }
+
+  if((interrupts & UA5_EIR_RX_FIFO_TIMEOUT_INT_PENDING) != 0) {
+    *input = __inb(UA4_RX_DATA) & 0x7f;
+    read_characters = 1;
+  }
+
+  /* if this trips, then the transmit fifo is empty */
+  if((interrupts & UA4_EIR_TX_INT_PENDING) != 0) {
+    __outb(UA4_TXD, output_buffer[output_index]);
+  }
+
+  if((interrupts & UA4_EIR_MODEM_STATUS_INT_PENDING) != 0) {
+  }
+  
+  __outw( PIC_MASTER_CMD_PORT, PIC_EOI );
 }
 
 /**
